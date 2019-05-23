94609cd8b49dabf0da054f7b147f03958f19bd69
jay194 <jayp379@gmail.com>
2019-05-23 15:33:58 -0700

    first commit

---
+++ src/main/java/com/unittestmaven/MavenUnitTest/MagicBuilder.java
@@ -0,0 +1,10 @@
+package com.unittestmaven.MavenUnitTest;
+
+
+public class MagicBuilder {
+
+    public static int getLucky() {
+        return 7;
+    }
+
+}
---
+++ src/main/java/com/unittestmaven/MavenUnitTest/MessageBuilder.java
@@ -0,0 +1,15 @@
+
+package com.unittestmaven.MavenUnitTest;
+
+
+public class MessageBuilder {
+    public static String getHelloWorld(){
+        return "hello world";
+    }
+
+    public static int getNumber10(){
+        return 10;
+    }
+
+}
+
